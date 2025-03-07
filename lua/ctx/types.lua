ffe3452ec28904198d9e4c693a61fd0557854c93
S1M0N38 <bertolottosimone@gmail.com>
2025-03-07 18:48:52 +0100

    feat(types): update item type using snacks def

--- lua/ctx/types.lua
+++ lua/ctx/types.lua
@@ -43,10 +43,22 @@
 ---@class Ctx.Items
 ---@field selection fun(): Ctx.Items.Selection? Selected lines as quickfix/loclist item
 
+---Represents an item in a Neovim quickfix/loclist.
 ---@class Ctx.Items.Base
----@field bufnr number Buffer number
----@field lnum number First line number
----@field end_lnum number? End line number
+---@field bufnr? number The buffer number where the item originates.
+---@field filename? string
+---@field lnum number The start line number for the item.
+---@field end_lnum? number The end line number for the item.
+---@field pattern string A pattern related to the item. It can be a search pattern or any relevant string.
+---@field col? number The column number where the item starts.
+---@field end_col? number The column number where the item ends.
+---@field module? string Module information (if any) associated with the item.
+---@field nr? number A unique number or ID for the item.
+---@field text? string A description or message related to the item.
+---@field type? string The type of the item. E.g., "W" might stand for "Warning".
+---@field valid number A flag indicating if the item is valid (1) or not (0).
+---@field user_data? any Any user data associated with the item.
+---@field vcol? number Visual column number. Indicates if the column number is a visual column number (when set to 1) or a byte index (when set to 0).
 
 ---@class Ctx.Items.Selection: Ctx.Items.Base
 ---@field text string First 80 characters of the selected lines
